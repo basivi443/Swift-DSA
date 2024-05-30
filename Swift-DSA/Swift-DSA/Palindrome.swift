28ce048ebc0a2cf4c0422b73ca99f948e5dc62dd
basivireddy <basivi.info016@gmail.com>
2024-05-30 10:01:36 +0530

    check palindrom if gives an array

---
+++ Swift-DSA/Swift-DSA/Palindrome.swift
@@ -0,0 +1,22 @@
+//
+//  Palindrome.swift
+//  Swift-DSA
+//
+//  Created by Basivi Reddy on 30/05/24.
+//
+
+import Foundation
+class Palindrome{
+    
+    func getPalindrome(numbers:[Int]){
+        var start = 0
+        var end = numbers.count - 1
+        while start < end{
+            if numbers[start] == numbers[end]{
+                print("\(numbers[start]) : \(numbers[end])")
+                start = start + 1
+                end = end - 1
+            }
+        }
+    }
+}
--- Swift-DSA/Swift-DSA/ViewController.swift
+++ Swift-DSA/Swift-DSA/ViewController.swift
@@ -11,7 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        var solution = Palindrome()
+        let value = solution.getPalindrome(numbers: [1,2,3,4,3,2,1])
+        print(value)
     }
 
 
