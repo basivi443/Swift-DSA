4a4603e9d45cfacca8442a2a1ead2e3ae165045c
basivireddy <basivi.info016@gmail.com>
2024-11-06 23:59:40 +0530

    LongestCommonPrefix

---
+++ Swift-DSA/Swift-DSA/LongestCommonPrefix.swift
@@ -0,0 +1,25 @@
+//
+//  LongestCommonPrefix.swift
+//  Swift-DSA
+//
+//  Created by Basivi Reddy on 06/11/24.
+//
+
+import UIKit
+
+class LongestCommonPrefix {
+    func longestCommonPrefix(_ strs: [String]) -> String {
+        var key = ""
+        let tem = Array(strs[0])
+        for i in 0..<tem.count {
+            key.append(tem[i])
+            for item in strs {
+                if !item.hasPrefix(key) {
+                    return String(key.dropLast(1))
+                }
+            }
+        }
+        
+        return key
+    }
+}
